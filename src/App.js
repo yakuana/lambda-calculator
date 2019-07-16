1e357ccb5e7ccbd97aa036c06ecd2c276aee2c71
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-07-16 17:47:11 -0500

    WIP: All Buttons Created. App.scss file created and some style has been added.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
+
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
+import Operators from './components/ButtonComponents/OperatorButtons/Operators';
+import Specials from './components/ButtonComponents/SpecialButtons/Specials';
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
@@ -18,6 +22,12 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <div>
+          <Numbers />
+          <Operators />
+        </div>
+        
+        <Specials />
       </div>
     </div>
   );
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
+import { NumberButton } from "./NumberButton"; 
 
 //Import your array data to from the provided data file
+import { numbers } from '../../../data';
+
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numberState] = useState(numbers);
+  
   return (
-    <div>
+    <div className = "number-container">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+
+      {numberState.map((number) => {return <NumberButton buttonNum = {number}/>})}
     </div>
   );
 };
+
+export default Numbers; 
